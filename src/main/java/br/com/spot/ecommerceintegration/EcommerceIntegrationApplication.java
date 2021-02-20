d4472f37ee390cb101250b34ba811bd86937d1c4
Gil Brandao <gcbrandao@gmail.com>
2021-02-20 20:46:31 -0300

    primeiro commit

---
+++ src/main/java/br/com/spot/ecommerceintegration/EcommerceIntegrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.spot.ecommerceintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/spot/ecommerceintegration/EcommerceIntegrationApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.spot.ecommerceintegration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceIntegrationApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
